d531e37a94d31eb1642b61979551202c79e6a240
Sides <chris.sides@lendinghome.com>
2018-05-21 17:18:14 -0700

    Grid layout, color mapping, css basics

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,2 +1,12 @@
 module GameHelper
+    def colorMapper(color)
+        colorMap = { 1 => 'red',
+            2 => 'blue',
+            3 => 'green',
+            4 => 'white',
+            5 => 'black',
+            6 => 'purple',
+        }
+        colorMap[color]
+    end  
 end
