3165d5b3f4dd04f20e39bf86e593e2f1fbbd4691
Sides <chris.sides@lendinghome.com>
2018-05-29 16:23:30 -0700

    Login capabilities, reusable nav bar

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,4 +1,5 @@
 class LandingPageController < ApplicationController
     def index
+        @user = User.new
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180518204123) do
+ActiveRecord::Schema.define(version: 20180529175851) do
 
   create_table "high_scores", force: :cascade do |t|
     t.string "name"
     t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
