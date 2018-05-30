5011bac251caf0546b51b0625f7b57ad114d5e49
Sides <chris.sides@lendinghome.com>
2018-05-30 16:53:41 -0700

    saved games page

--- app/controllers/saved_page_controller.rb
+++ app/controllers/saved_page_controller.rb
@@ -1,11 +1,10 @@
 class SavedPageController < ApplicationController
     def index
         @saved_games = SavedGame.where(:owner => nil)
-        
+
         if current_user
             @saved_games += SavedGame.where(:owner => current_user.username)
         end
-
-        puts @saved_games
+        @saved_games.sort!{ |a,b| a.owner <=> b.owner || (b && 1) || -1 }
     end
 end
